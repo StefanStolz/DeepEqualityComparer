81c3446d8af530bee8f3bf450c9e2490353f1351
Stefan Stolz <Stefan.Stolz@gmx.de>
2017-06-09 16:50:09 +0200

    Pick Nuget-Package-Version from C# Project Use VS2017 Toolchain

--- source/DeepEqualityComparer/DeepEqualityComparer/buildNugetPackage.bat
+++ source/DeepEqualityComparer/DeepEqualityComparer/buildNugetPackage.bat
@@ -1,3 +1,3 @@
 @echo off
-msbuild DeepEqualityComparer.csproj /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" DeepEqualityComparer.csproj /p:Configuration=Release
 nuget pack DeepEqualityComparer.csproj -Prop configuration=Release
