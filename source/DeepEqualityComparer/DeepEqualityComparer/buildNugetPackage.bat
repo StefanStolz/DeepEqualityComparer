2976edf16d43cba90054763cf03ffb438bcb309b
Stefan Stolz <Stefan.Stolz@gmx.de>
2017-08-21 20:07:42 +0200

    2.0.2-alpha

--- source/DeepEqualityComparer/DeepEqualityComparer/buildNugetPackage.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" DeepEqualityComparer.csproj /p:Configuration=Release
-nuget pack DeepEqualityComparer.csproj -Prop configuration=Release
