cd3259779c83b23c5cb29568c0f7729665437f8e
Stefan Stolz <Stefan.Stolz@gmx.de>
2017-08-24 14:28:33 +0200

    Compare Immtable Arrays

---
+++ source/Integration/UsageOfNugetPackage/CompareImmutableArrays.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+using deepequalitycomparer;
+
+using NUnit.Framework;
+
+// ReSharper disable ConsiderUsingAsyncSuffix
+
+namespace UsageOfNugetPackage
+{
+    [TestFixture]
+    public sealed class CompareImmutableArrays
+    {
+        [Test]
+        public void EmtpyArraysWithBytesAreEqual()
+        {
+            var c1 = ImmutableArray<byte>.Empty;
+            var c2 = ImmutableArray<byte>.Empty;
+
+            var result = DeepEqualityComparer.Default.Equals(c1, c2);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void ComapreByteArraysWithEqualContent()
+        {
+            var source = new byte[] { 1, 2, 3, 4, 5 };
+
+            var c1 = source.ToImmutableArray();
+            var c2 = source.ToImmutableArray();
+
+
+            var result = DeepEqualityComparer.Default.Equals(c1, c2);
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
+
